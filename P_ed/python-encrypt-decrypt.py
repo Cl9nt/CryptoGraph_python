79783d1b1f3bf07e31d72adc3cd3d3e5d7bd15b3
muth <muthunguclintn@gmail.com>
2021-07-01 22:51:45 +0300

    python-encrypt-decrypt

---
+++ P_ed/python-encrypt-decrypt.py
@@ -0,0 +1,23 @@
+## Using pyAesCrypt
+
+import pyAesCrypt
+
+# encryption / decryption buffer size - 64K
+
+bufferSize = 64 *1024
+
+password = "foopassword"
+
+# encrypt
+
+pyAesCrypt.encryptFile("data.txt", "data.txt.aes", password, bufferSize)
+
+# decrypt
+
+pyAesCrypt.decryptFile("data.txt.aes", "dataout.txt", password, bufferSize)
+
+## Using pyCrypto ##
+
+import os, random, struct
+
+from Crypto.Cipher import AES
